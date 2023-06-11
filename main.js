b8b7cc6ecb7b6bd656b4e6ca2c4d8d1ec9ae7590
Emzzy241 <emzzyoluwole@gmail.com>
2023-06-11 03:51:15 +0100

    adding new stuffs

--- main.js
+++ main.js
@@ -21,7 +21,8 @@ $(document).ready(() => {
         if (userCourse === "11") {
             $(".myCoure2").hide();
 
-            let gpa;
+            let oneCoursegpa;
+            let twoCoursegpa;
 
             $(".course-info").fadeOut();
 
@@ -76,50 +77,50 @@ $(document).ready(() => {
                 // For a 1 unit course and several grades
                 if(userGrade1 == "A" && userUnits1 == "1")
                 {
-                    gpa = 1 * 5;
+                    oneCoursegpa = 1 * 5;
 
                     $(".myResult").text("You had an A in a 1 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
                  
                 }
                 else if(userGrade1 == "B" && userUnits1 == "1")
                 {
-                    gpa = 1 * 4;
+                    oneCoursegpa = 1 * 4;
 
                     $(".myResult").text("You had a B in a 1 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
                 else if(userGrade1 == "C" && userUnits1 == "1")
                 {
-                    gpa = 1 * 3;
+                    oneCoursegpa = 1 * 3;
 
                     $(".myResult").text("You had a C in a 1 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
                 else if(userGrade1 == "D" && userUnits1 == "1")
                 {
-                    gpa = 1 * 2;
+                    oneCoursegpa = 1 * 2;
 
                     $(".myResult").text("You had a D in a 1 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
                 else if(userGrade1 == "E" && userUnits1 == "1")
                 {
-                    gpa = 1 * 1;
+                    oneCoursegpa = 1 * 1;
 
                     $(".myResult").text("You had an E in a 1 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
                 else if(userGrade1 == "F" && userUnits1 == "1")
                 {
-                    gpa = 1 * 0;
+                    oneCoursegpa = 1 * 0;
 
                     $(".myResult").text("You had an F in a 1 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
 
@@ -127,50 +128,50 @@ $(document).ready(() => {
 
                 if(userGrade1 == "A" && userUnits1 == "2")
                 {
-                    gpa = 2 * 5;
+                    oneCoursegpa = 2 * 5;
 
                     $(".myResult").text("You had an A in a 2 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
                  
                 }
                 else if(userGrade1 == "B" && userUnits1 == "2")
                 {
-                    gpa = 2 * 4;
+                    oneCoursegpa = 2 * 4;
 
                     $(".myResult").text("You had a B in a 2 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
                 else if(userGrade1 == "C" && userUnits1 == "2")
                 {
-                    gpa = 2 * 3;
+                    oneCoursegpa = 2 * 3;
 
                     $(".myResult").text("You had a C in a 2 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
                 else if(userGrade1 == "D" && userUnits1 == "2")
                 {
-                    gpa = 2 * 2;
+                    oneCoursegpa = 2 * 2;
 
                     $(".myResult").text("You had a D in a 2 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
                 else if(userGrade1 == "E" && userUnits1 == "2")
                 {
-                    gpa = 2 * 1;
+                    oneCoursegpa = 2 * 1;
 
                     $(".myResult").text("You had an E in a 2 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
                 else if(userGrade1 == "F" && userUnits1 == "2")
                 {
-                    gpa = 2 * 0;
+                    oneCoursegpa = 2 * 0;
 
                     $(".myResult").text("You had an E in a 2 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
 
@@ -179,54 +180,54 @@ $(document).ready(() => {
                 
                 if(userGrade1 == "A" && userUnits1 == "3")
                 {
-                    gpa = 3 * 5;
+                    oneCoursegpa = 3 * 5;
 
                     $(".myResult").text("You had an A in a 3 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
                  
                 }
                 else if(userGrade1 == "B" && userUnits1 == "3")
                 {
-                    gpa = 3 * 4;
+                    oneCoursegpa = 3 * 4;
 
                     $(".myResult").text("You had a B in a 3 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
                 else if(userGrade1 == "C" && userUnits1 == "3")
                 {
-                    gpa = 3 * 3;
+                    oneCoursegpa = 3 * 3;
 
                     $(".myResult").text("You had a C in a 3 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
                 else if(userGrade1 == "D" && userUnits1 == "3")
                 {
-                    gpa = 3 * 2;
+                    oneCoursegpa = 3 * 2;
 
                     $(".myResult").text("You had a D in a 3 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
                 else if(userGrade1 == "E" && userUnits1 == "3")
                 {
-                    gpa = 3 * 1;
+                    oneCoursegpa = 3 * 1;
 
                     $(".myResult").text("You had an E in a 3 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
                 else if(userGrade1 == "F" && userUnits1 == "3")
                 {
-                    gpa = 3 * 0;
+                    oneCoursegpa = 3 * 0;
 
                     $(".myResult").text("You had an E in a 3 unit course")
-                    $(".myResult2").text("and your grade point per course is: " + gpa);
+                    $(".myResult2").text("and your grade point per course is: " + oneCoursegpa);
 
                 }
 
-                let gradePCourse1 = gpa;
+                let gradePCourse1 = oneCoursegpa;
 
                 $(".continue1").fadeOut();
                 $(".continue2").fadeIn();
@@ -262,6 +263,167 @@ $(document).ready(() => {
                 let userUnits2 = $(".unit2").find(":selected").val();
                 let userGrade2 = $(".grade2").find(":selected").val();
             
+
+                   
+                // For a 1 unit course and several grades
+                if(userGrade2 == "A" && userUnits2 == "1")
+                {
+                    twoCoursegpa = 1 * 5;
+
+                    $(".mySecondResult").text("You had an A in a 1 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+                 
+                }
+                else if(userGrade2 == "B" && userUnits2 == "1")
+                {
+                    twoCoursegpa = 1 * 4;
+
+                    $(".mySecondResult").text("You had a B in a 1 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+                else if(userGrade2 == "C" && userUnits2 == "1")
+                {
+                    twoCoursegpa = 1 * 3;
+
+                    $(".mySecondResult").text("You had a C in a 1 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+                else if(userGrade2 == "D" && userUnits2 == "1")
+                {
+                    twoCoursegpa = 1 * 2;
+
+                    $(".mySecondResult").text("You had a D in a 1 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+                else if(userGrade2 == "E" && userUnits2 == "1")
+                {
+                    twoCoursegpa = 1 * 1;
+
+                    $(".mySecondResult").text("You had an E in a 1 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+                else if(userGrade2 == "F" && userUnits2 == "1")
+                {
+                    twoCoursegpa = 1 * 0;
+
+                    $(".mySecondResult").text("You had an F in a 1 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+
+                // For 2 units now
+
+                if(userGrade2 == "A" && userUnits2 == "2")
+                {
+                    twoCoursegpa = 2 * 5;
+
+                    $(".mySecondResult").text("You had an A in a 2 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+                 
+                }
+                else if(userGrade2 == "B" && userUnits2 == "2")
+                {
+                    twoCoursegpa = 2 * 4;
+
+                    $(".mySecondResult").text("You had a B in a 2 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+                else if(userGrade2 == "C" && userUnits2 == "2")
+                {
+                    twoCoursegpa = 2 * 3;
+
+                    $(".mySecondResult").text("You had a C in a 2 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+                else if(userGrade2 == "D" && userUnits2 == "2")
+                {
+                    twoCoursegpa = 2 * 2;
+
+                    $(".mySecondResult").text("You had a D in a 2 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+                else if(userGrade2 == "E" && userUnits2 == "2")
+                {
+                    twoCoursegpa = 2 * 1;
+
+                    $(".mySecondResult").text("You had an E in a 2 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+                else if(userGrade2 == "F" && userUnits2 == "2")
+                {
+                    twoCoursegpa = 2 * 0;
+
+                    $(".mySecondResult").text("You had an E in a 2 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+
+                // For 3 unit courses now
+
+                
+                if(userGrade2 == "A" && userUnits2 == "3")
+                {
+                    twoCoursegpa = 3 * 5;
+
+                    $(".mySecondResult").text("You had an A in a 3 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+                 
+                }
+                else if(userGrade2 == "B" && userUnits2 == "3")
+                {
+                    twoCoursegpa = 3 * 4;
+
+                    $(".mySecondResult").text("You had a B in a 3 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+                else if(userGrade2 == "C" && userUnits2 == "3")
+                {
+                    twoCoursegpa = 3 * 3;
+
+                    $(".mySecondResult").text("You had a C in a 3 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+                else if(userGrade2 == "D" && userUnits2 == "3")
+                {
+                    twoCoursegpa = 3 * 2;
+
+                    $(".mySecondResult").text("You had a D in a 3 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+                else if(userGrade2 == "E" && userUnits2 == "3")
+                {
+                    twoCoursegpa = 3 * 1;
+
+                    $(".mySecondResult").text("You had an E in a 3 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+                else if(userGrade2 == "F" && userUnits2 == "3")
+                {
+                    twoCoursegpa = 3 * 0;
+
+                    $(".mySecondResult").text("You had an E in a 3 unit course")
+                    $(".mySecondResult2").text("and your grade point per course in your second course is: " + twoCoursegpa);
+
+                }
+
+                let gradePCourse1 = twoCoursegpa;
+
+                $(".continue2").fadeOut();
+                $("#Next-2").fadeIn();
+
+
                 
             })
 
